9db2e65e5adaf229b891fe41781bcb2e40de190c
Lorenzo Peña <lorinkoz@gmail.com>
2023-04-19 20:21:51 +0200

    Applied pre-commit

--- djsc_sandbox/myapp/models.py
+++ djsc_sandbox/myapp/models.py
@@ -5,17 +5,14 @@ from .choices import MEDAL_TYPES, MEDIA_CHOICES, YEAR_IN_SCHOOL_CHOICES
 
 
 class ModelA(models.Model):
-
     year_in_school = models.CharField(max_length=2, blank=True, choices=YEAR_IN_SCHOOL_CHOICES)
 
 
 class ModelB(models.Model):
-
     year_in_school = models.CharField(max_length=2, blank=True, choices=YEAR_IN_SCHOOL_CHOICES[:-1])
     media = models.CharField(max_length=10, blank=True, choices=MEDIA_CHOICES)
 
 
 class ModelC(models.Model):
-
     medals = multiselect.MultiSelectField(blank=True, choices=MEDAL_TYPES)
     media = models.CharField(max_length=10, blank=True, choices=MEDIA_CHOICES)
